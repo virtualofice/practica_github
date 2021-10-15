4c837b26562aedd4e8fdca269b9244a1ecf8eab0
virtualofice <virtualofice@hotmail.com>
2021-10-14 22:55:01 -0300

    agrega clase perro

---
+++ src/gitegg/GitEgg.java
@@ -0,0 +1,12 @@
+package gitegg;
+
+public class GitEgg {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+
+}
---
+++ src/gitegg/entidades/Perro.java
@@ -0,0 +1,21 @@
+package gitegg.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author chacho
+ */
+public class Perro {  // clase para ejemplo de GitHub
+
+    private String nombre;
+
+    private String raza;
+
+    private String sexo;
+
+    private Date fechaNacimiento;
+
+    private String color;
+
+}
